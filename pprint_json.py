c731f0336be83437f3780697f1ab8c82177e584b
MacPro <artem@MacPro.local>
2017-07-06 22:08:00 +0300

    Make functions to get script working

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,17 @@
-import json
+import json, sys
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, 'r') as handle:
+        return json.load(handle)
+
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, ensure_ascii=False, indent=4, sort_keys=None))
 
 
 if __name__ == '__main__':
-    pass
+    # print(sys.argv[1])
+    data = load_data(sys.argv[1])
+    pretty_print_json(data)
