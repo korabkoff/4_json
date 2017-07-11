69ff5ae55e3826ad3cd9067369befe452f521e84
korabkoff <korabkoff@gmail.com>
2017-07-11 17:41:16 +0300

    input/ proses / output convention now applied
    print under if __name__ == '__main__'
    
    simple arg parsing in case of missed file path argument
    
    ensure None returned if something goes wrong from every function

--- pprint_json.py
+++ pprint_json.py
@@ -3,17 +3,39 @@ import sys
 import os
 
 
-def load_json_data(filepath):
-    if not os.path.exists(filepath):
+def get_commandline_arg(index):
+    if len(sys.argv) == index+1:
+        return sys.argv[index]
+    else:
         return None
-    with open(filepath, 'r') as json_file:
+
+
+def load_json_data(file_path):
+
+    if not file_path:
+        return None
+    elif not os.path.exists(file_path):
+        return None
+
+    with open(file_path, 'r') as json_file:
+        if not json_file.read(1):  # in case of empty file
+            return None
+
+        json_file.seek(0)  # Return to the start of content
         return json.load(json_file)
 
 
 def pretty_print_json(data):
-    print(json.dumps(data, ensure_ascii=False, indent=4, sort_keys=None))
+    if not data:
+        return None
+
+    return json.dumps(data, ensure_ascii=False, indent=4, sort_keys=None)
 
 
 if __name__ == '__main__':
-    json_content = load_json_data(sys.argv[1])
-    pretty_print_json(json_content)
+
+    json_file_path = get_commandline_arg(1)
+
+    json_content = load_json_data(json_file_path)
+
+    print(pretty_print_json(json_content))
